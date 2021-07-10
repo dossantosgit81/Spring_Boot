7a186b4d1f0aeffb6d06193e3441d6170e5e77cb
Rafael Mendes dos Santos <dossantosgit81@gmail.com>
2021-07-10 16:50:25 -0300

    Atualizando recurso spring boot

--- src/main/java/com/mendessolutions/vendas/rest/controller/ClienteController.java
+++ src/main/java/com/mendessolutions/vendas/rest/controller/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -52,6 +53,21 @@ public class ClienteController {
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@PutMapping("/api/clientes/{id}")
+	@ResponseBody
+	public ResponseEntity update(@RequestBody Cliente cliente, 
+			@PathVariable Integer id ) {
+		return clientes
+				.findById(id)
+				.map(clienteExistente -> {
+			cliente.setId(clienteExistente.getId());
+			clientes.save(cliente);
+			return ResponseEntity.noContent().build();
+			
+		}).orElseGet(() -> ResponseEntity.notFound().build());
 	}
 	
 	
